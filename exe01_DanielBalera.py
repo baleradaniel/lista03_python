40cb4e8a92f83e0d0f25b5f951fb74d3bee72404
baleradaniel <danielbalera021@gmail.com>
2025-02-12 20:49:19 -0300

    python

---
+++ exe01_DanielBalera.py
@@ -0,0 +1,12 @@
+"""Peça dois números. Se o primeiro for maior que o segundo, exiba primeiro o segundo número e depois o primeiro número,
+ caso contrário, mostre primeiro o primeiro número e depois o segundo."""
+numero1 = int(input('Insira o primeiro numero: '))
+numero2 = int(input('Insira o segundo numero: '))
+
+if numero1 > numero2:
+    print(numero2, numero1)
+    print('Daniel Balera')
+else:
+    print(numero1, numero2)
+    print('Daniel Balera')
+    
